6dab2febfc4d974636b74b606b9e447342e8d6ec
Vanessa Emerick <vavarooks123@gmail.com>
2022-11-21 18:02:11 -0800

    Made about page

--- client/src/views/About.jsx
+++ client/src/views/About.jsx
@@ -8,6 +8,9 @@ const About = () => {
         <>
             <div className='container d-block mx-auto w-50 my-3 p-3' >
                 <h1>About</h1>
+                <h2>Meet the Team</h2>
+                <p>Evelyn Emerick, Artist</p>
+                <p>Vanessa Emerick, Programmer</p>
             </div>
         </>
     )
--- client/src/views/Contact.jsx
+++ client/src/views/Contact.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 const Contact = () => {
@@ -8,6 +8,17 @@ const Contact = () => {
         <>
             <div className='container d-block mx-auto w-50 my-3 p-3' >
                 <h1>Contact</h1>
+                <form method="post" action="mailto:youremail@youremail.com">
+                    <div className="mb-3">
+                        <label className="form-label">Email address</label>
+                        <input type="email" className="form-control" placeholder="name@example.com"/>
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label">Message</label>
+                        <textarea className="form-control" rows="3"></textarea>
+                    </div>
+                    <button className='btn btn-primary'>Submit</button>
+                </form>
             </div>
         </>
     )
