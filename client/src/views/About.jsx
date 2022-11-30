1a26202e63d895e6b5332be203833479ae2b1e07
Vanessa Emerick <vavarooks123@gmail.com>
2022-11-30 14:03:04 -0800

    Adding in about photos

--- client/src/views/About.jsx
+++ client/src/views/About.jsx
@@ -8,18 +8,22 @@ const About = () => {
         <>
             <div className='container d-block mx-auto w-50 my-3 p-3' >
                 <h2>Meet the Team</h2>
-                <div>
-                    <img src="..." class="rounded" alt="..."/>
-                    <p>Evelyn Emerick, Artist</p>
-                    <p>Graduate of Art Academy</p>
-                    <p>My Sister :0</p>
-                </div>
                 <div className='container card img-size'>
                     <img src={process.env.PUBLIC_URL + '/selfie.jpg'} class="rounded img-fluid" alt="vanessa-emerick"/>
+                        <div className='card-title'>
+                            <p>Evelyn Emerick, Artist</p>
+                        </div>
                     <div className='card-body'>
-                        <p>Vanessa Emerick, Programmer</p>
-                        <p>Graduate of UC Extention Coding Program</p>
-                        <p>Graduate of Sunnyvale Coding Dojo Program</p>
+                        <p>Graduate of </p>
+                    </div>
+                </div>
+                <div className='container card img-size'>
+                    <img src={process.env.PUBLIC_URL + '/selfie2.jpg'} class="rounded img-fluid" alt="evelyn-emerick"/>
+                        <div className='card-title'>
+                            <p>Evelyn Emerick, Programmer</p>
+                        </div>
+                    <div className='card-body'>
+                        <p>Graduate of </p>
                     </div>
                 </div>
             </div>
